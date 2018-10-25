59ae9b89a6ffc6eb8820c9235399ef75b1c01f2b
AnaelBerrouet <anael.jberrouet@gmail.com>
2018-10-25 02:08:19 +0000

    summed

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+
+  let element = 0;
+
+  if(array.length > 0){
+    element = array.shift();
+    if(Array.isArray(element)){
+      element = sumItems(element);
+    }
+  } else {
+    return 0;
+  }
+
+  return element + sumItems(array)
+
 }
 
 module.exports = sumItems;
